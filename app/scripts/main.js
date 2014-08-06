53c95a227bf97862e6c75a71423cdaedc32f7b8b
Camille Reynders <camille.reynders@gmail.com>
2014-08-06 12:34:04 +0200

    Swap underscore for lodash
    Uses ugly hack from http://stackoverflow.com/questions/24231184/bower-override-dependency

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,5 +4,6 @@ console.log('--( MAIN )--');
 console.log('Backbone loaded', 'undefined' !== typeof Backbone );
 console.log('Backbone.Marionette loaded', 'undefined' !== typeof Backbone.Marionette );
 console.log('Jquery loaded', 'undefined' !== typeof $ );
+console.log('Underscore/lodash loaded', 'undefined' !== typeof _);
 var temp = require('./temp.js');
 console.log(temp.temp);
