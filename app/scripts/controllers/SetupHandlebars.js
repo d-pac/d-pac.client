6f5151fc577002bc179043ad8fd7522e5b15cba5
Camille Reynders <camille.reynders@gmail.com>
2014-08-26 12:21:55 +0200

    Allow dynamic loading of localized hbs templates

--- app/scripts/controllers/SetupHandlebars.js
+++ app/scripts/controllers/SetupHandlebars.js
@@ -1,10 +1,11 @@
 'use strict';
 var debug = require( 'bows' )( 'dpac:commands' );
-var Handlebars = require( "hbsfy/runtime" );
+var hbsfy = require( "hbsfy/runtime" );
 module.exports = function SetupHandlebars(){
-    debug('SetupHandlebars#constructor');
-    Handlebars.registerHelper( 't', function( i18n_key,
-                                              options ){
+    debug( 'SetupHandlebars#constructor' );
+
+    function translate( i18n_key,
+                        options ){
         var context = options.data.root;
         var opts = (context)
             ? i18n.functions.extend( options.hash, context )
@@ -15,5 +16,9 @@ module.exports = function SetupHandlebars(){
 
         var result = i18n.t( i18n_key, opts );
         return new Handlebars.SafeString( result );
-    } );
+    }
+
+    //TODO: ugly, ugly, ugly, figure out a way how to combine both
+    Handlebars.registerHelper( 't', translate );
+    hbsfy.registerHelper( 't', translate );
 }
