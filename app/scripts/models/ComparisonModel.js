a5c936a194755e4674736dad17a7474304eb6707
Camille Reynders <camille.reynders@gmail.com>
2014-09-24 10:27:32 +0200

    Add accessors to ComparisonModel

--- app/scripts/models/ComparisonModel.js
+++ app/scripts/models/ComparisonModel.js
@@ -7,6 +7,14 @@ module.exports = Backbone.NestedModel.extend( {
         debug( '#initialize' );
     },
 
+    toJSON : function(){
+        //Holy crap, do we really need to do this?
+        var attr = Backbone.Model.prototype.toJSON.call( this );
+        attr.title = this.get( 'assessment.title' );
+        attr.description = this.get( 'assessment.description' );
+        return attr;
+    },
+
     isActive : function(){
         return this.get( 'comparison.active' );
     }
