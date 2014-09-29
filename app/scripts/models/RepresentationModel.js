242e85d450a6798e1473bb75360c434023176cf9
Camille Reynders <camille.reynders@gmail.com>
2014-09-29 10:56:57 +0200

    Add RepresentationModel

---
+++ app/scripts/models/RepresentationModel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var debug = require( 'debug' )( 'dpac:models', '[RepresentationModel]' );
+module.exports = Backbone.Model.extend( {
+    defaults : {
+        url      : "",
+        note     : "",
+        passfail : ""
+    },
+
+    initialize : function(){
+        debug( '#initialize' );
+    }
+} );
