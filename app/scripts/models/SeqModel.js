2b94d610f96ea806a0d212793485cea83bdbe1fb
Camille Reynders <camille.reynders@gmail.com>
2014-09-29 10:57:20 +0200

    Add SeqModel

---
+++ app/scripts/models/SeqModel.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var debug = require( 'debug' )( 'dpac:models', '[SeqModel]' );
+module.exports = Backbone.Model.extend( {
+    defaults : {
+        value : -1
+    },
+
+    initialize : function(){
+        debug( '#initialize' );
+    }
+} );
