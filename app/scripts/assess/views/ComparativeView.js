7d7cdf081acb78cb332fdaa53e514abcdedb6bc5
Camille Reynders <camille.reynders@gmail.com>
2014-10-08 11:46:57 +0200

    hookup wysiwyg editor to comparative view

--- app/scripts/assess/views/ComparativeView.js
+++ app/scripts/assess/views/ComparativeView.js
@@ -1,21 +1,38 @@
 'use strict';
 
 var debug = require( 'debug' )( 'dpac:assess.views', '[ComparativeView]' );
-var tpl = require('./templates/ComparativeView.hbs');
+var tpl = require( './templates/ComparativeView.hbs' );
 
-module.exports = Marionette.ItemView.extend({
+module.exports = Marionette.ItemView.extend( {
     template : tpl,
+    ui       : {
+        editor     : "textarea",
+        saveButton : ".save-button"
+    },
+    events : {
+        "click @ui.saveButton" : "save"
+    },
+
     initialize : function(){
-        debug("#initialize");
+        debug( "#initialize" );
     },
 
     onRender : function(){
-        this.$('textarea' ).wysihtml5({ toolbar:{
-            "font-styles":false,
-            link:false,
-            image:false,
-            color:false,
-            size:"sm"
-        }});
+        this.ui.editor.wysihtml5( {
+            toolbar : {
+                "font-styles" : false,
+                link          : false,
+                image         : false,
+                color         : false,
+                size          : "sm"
+            }
+        } );
+    },
+    save : function(){
+        debug.debug('#save');
+        this.model.set({
+            comparativeFeedback :this.ui.editor.val()
+        });
+        this.trigger("comparativeFeedback:edited");
     }
-});
+} );
--- app/scripts/assess/views/WizardView.js
+++ app/scripts/assess/views/WizardView.js
@@ -4,8 +4,8 @@ var debug = require( 'debug' )( 'dpac:assess.views', '[WizardView]' );
 var tpl = require( './templates/WizardView.hbs' );
 
 module.exports = Marionette.LayoutView.extend( {
-    template    : tpl,
-    regions     : {
+    template         : tpl,
+    regions          : {
         content : "#assessment-wizard-content"
     },
     collectionEvents : {
@@ -20,19 +20,20 @@ module.exports = Marionette.LayoutView.extend( {
         var factory = this[type + "Factory"];
 
         if( !factory ){
-            debug.error( 'view factory not found for type "' + type + "'");
+            debug.error( 'view factory not found for type "' + type + "'" );
             this.content.empty()
         }else{
             var view = factory();
             this.content.show( view );
             this.listenTo( view, 'representation:selected', this.gotoNext );
             this.listenTo( view, 'seq:edited', this.gotoNext );
+            this.listenTo( view, 'comparativeFeedback:edited', this.gotoNext );
         }
     },
 
     gotoNext : function(){
-        debug.debug('gotoNext');
-        this.stopListening(this.content.currentView);
+        debug.debug( 'gotoNext' );
+        this.stopListening( this.content.currentView );
         this.collection.selectNext();
     }
 } );
