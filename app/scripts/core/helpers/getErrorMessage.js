c7eb6aba9c38f5496e6bd8d4b0051fe82ba49b91
Camille Reynders <camille.reynders@gmail.com>
2014-10-15 10:37:11 +0200

    Fix incorrect error parsing

--- app/scripts/core/helpers/getErrorMessage.js
+++ app/scripts/core/helpers/getErrorMessage.js
@@ -1,7 +1,22 @@
 'use strict';
 
 module.exports = function getErrorMessage(err){
-    var messageKey = S(err.reason.message).slugify().s;
+
+    if( err.explanation ){
+        var explanation = err.explanation;
+        if(!_.isArray( explanation )){
+            explanation = [explanation];
+        }
+        var message = [];
+        _.each(explanation, function(){
+            var messageKey = S(explanation).slugify().s;
+            message.push(i18n.t( ["errors:"+messageKey, explanation] ));
+        });
+
+    }else{
+        var messageKey = S(err.message).slugify().s;
+        message = [i18n.t( ["errors:"+messageKey, err.message] )];
+    }
     //console.log(messageKey);
-    return i18n.t( ["errors:"+messageKey, err.reason.message] );
+    return message.join("<br/>");
 };
