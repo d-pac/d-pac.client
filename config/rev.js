b62644195c93f0ea5bfaac4e7171a1c38fdf75a7
Camille Reynders <camille.reynders@gmail.com>
2014-10-24 11:46:10 +0200

    Fix #27; disable revving for images

--- config/rev.js
+++ config/rev.js
@@ -3,7 +3,6 @@ module.exports = {
     files: {
       src: ['<%= config.dist %>/scripts/{,*/}*.js',
         '<%= config.dist %>/styles/{,*/}*.css',
-        '<%= config.dist %>/images/{,*/}*.*',
         '<%= config.dist %>/styles/fonts/{,*/}*.*',
         '<%= config.dist %>/*.{ico,png}'
       ]
