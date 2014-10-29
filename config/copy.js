2a65222f0df8ed5deb172b3de2ae80698c065055
Camille Reynders <camille.reynders@gmail.com>
2014-10-29 15:37:26 +0100

    Move dummy pdf and integrate into build process

--- config/copy.js
+++ config/copy.js
@@ -14,6 +14,7 @@ module.exports = {
                     'styles/fonts/{,*/}*.*',
                     'locales/**/*.*',
                     'viewerjs/**',
+                    'pdf/**/*.pdf',
                     'swf/**/*.*',
                     'scripts/core/config.json'
                 ]
