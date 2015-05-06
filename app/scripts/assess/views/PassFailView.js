e9f4392322291649f9946e1e07259308fbd4c917
Camille Reynders <camille.reynders@gmail.com>
2015-05-06 11:41:33 +0200

    Use jquery module instead of global

--- app/scripts/assess/views/PassFailView.js
+++ app/scripts/assess/views/PassFailView.js
@@ -1,62 +1,64 @@
 'use strict';
 var _ = require( 'underscore' );
+var i18n = require( 'i18next' );
 
 var debug = require( 'debug' )( 'dpac:assess.views', '[PassFailView]' );
 var tpl = require( './templates/PassFailView.hbs' );
 
 module.exports = Marionette.ItemView.extend( {
-    template  : tpl,
-    ui        : {
-        saveButton : ".save-button",
-        leftButtons : "input:radio[name ='leftPassFail']",
-        rightButtons : "input:radio[name ='rightPassFail']"
+    template: tpl,
+    ui: {
+        saveButton: ".save-button",
+        leftButtons: "input:radio[name ='leftPassFail']",
+        rightButtons: "input:radio[name ='rightPassFail']"
     },
-    events    : {
-        "click @ui.saveButton" : "save",
-        "click @ui.leftButtons" : "valueClicked",
-        "click @ui.rightButtons" : "valueClicked"
+    events: {
+        "click @ui.saveButton": "save",
+        "click @ui.leftButtons": "valueClicked",
+        "click @ui.rightButtons": "valueClicked"
     },
-    className : "col-md-8 col-md-offset-2 column",
+    className: "col-md-8 col-md-offset-2 column",
 
-    initialize : function(){
+    initialize: function(){
         debug( "#initialize" );
 
         this.leftModel = this.collection.at( 0 );
         this.rightModel = this.collection.at( 1 );
     },
 
-    serializeData : function(){
-        var data = $.t( "assessment:comparisons.passfail.options", { returnObjectTrees : true } );
+    serializeData: function(){
+        var data = i18n.t( "assessment:comparisons.passfail.options", { returnObjectTrees: true } );
         var left = this.leftModel.get( 'passed' );
         var right = this.rightModel.get( 'passed' );
         if( left ){
-            data[left].left = true;
+            data[ left ].left = true;
         }
         if( right ){
-            data[right].right = true;
+            data[ right ].right = true;
         }
         return {
-            options : data
+            options: data
         };
     },
 
-    onRender : function(){
+    onRender: function(){
         var left = this.$( "input:radio[name ='leftPassFail']:checked" ).val();
-        var right= this.$( "input:radio[name ='rightPassFail']:checked" ).val();
-        this.setButtonState(left, right);
+        var right = this.$( "input:radio[name ='rightPassFail']:checked" ).val();
+        this.setButtonState( left, right );
     },
 
-    valueClicked : function(){
+    valueClicked: function(){
         var left = this.$( "input:radio[name ='leftPassFail']:checked" ).val();
-        var right= this.$( "input:radio[name ='rightPassFail']:checked" ).val();
-        this.setButtonState(left, right);
+        var right = this.$( "input:radio[name ='rightPassFail']:checked" ).val();
+        this.setButtonState( left, right );
     },
 
-    setButtonState : function(left, right){
-        this.$(this.ui.saveButton ).prop('disabled', !left || !right);
+    setButtonState: function( left,
+                              right ){
+        this.$( this.ui.saveButton ).prop( 'disabled', !left || !right );
     },
 
-    save : _.debounce( function(){
+    save: _.debounce( function(){
         debug.debug( '#save' );
         this.leftModel.set( 'passed', this.$( "input:radio[name ='leftPassFail']:checked" ).val() );
         this.rightModel.set( 'passed', this.$( "input:radio[name ='rightPassFail']:checked" ).val() );
--- app/scripts/core/controllers/SetupRemoteRequests.js
+++ app/scripts/core/controllers/SetupRemoteRequests.js
@@ -1,5 +1,6 @@
 'use strict';
 var _ = require( 'underscore' );
+var $ = require('jquery');
 
 var debug = require( 'debug' )( 'dpac:core.controllers', '[SetupRemoteRequests]' );
 var uuid = require( 'node-uuid' );
