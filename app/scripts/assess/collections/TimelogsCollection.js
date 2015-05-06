b26150da44939f2d6c3eb11707dab43025760445
Camille Reynders <camille.reynders@gmail.com>
2015-05-06 11:14:15 +0200

    Use mousetrap module instead of global

--- app/scripts/assess/collections/TimelogsCollection.js
+++ app/scripts/assess/collections/TimelogsCollection.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Mousetrap = require( "mousetrap" );
 var debug = require( 'debug' )( 'dpac:assess', '[TimelogsCollection]' );
 var teardown = require( '../mixins/teardown' );
 
@@ -7,51 +8,52 @@ var ModelClass = require( '../models/TimelogProxy' );
 
 module.exports = Backbone.Collection.extend( {
 
-    url   : "/timelogs",
-    model : ModelClass,
-    contextEvents : {
-        'assessment:teardown:requested' : "teardown",
-        'AuthService:signout:requested' : "stop"
+    url: "/timelogs",
+    model: ModelClass,
+    contextEvents: {
+        'assessment:teardown:requested': "teardown",
+        'AuthService:signout:requested': "stop"
     },
 
-    initialize : function( models, opts ){
+    initialize: function( models,
+                          opts ){
         debug( '#initialize' );
         Backbone.Select.One.applyTo( this, models );
 
-        opts = _.defaults({}, opts, {
-            interval : 5000,
-            shortcut : 'ctrl+alt+s'
-        });
+        opts = _.defaults( {}, opts, {
+            interval: 5000,
+            shortcut: 'ctrl+alt+s'
+        } );
         this.updateInterval = opts.interval;
         this.autoUpdateDisabled = false;
 
         var self = this;
-        Mousetrap.bind(opts.shortcut, function(){
-            self.autoUpdateDisabled = ! self.autoUpdateDisabled;
-            debug.warn('Toggling auto update to ', !self.autoUpdateDisabled);
-            if(self.autoUpdateDisabled){
+        Mousetrap.bind( opts.shortcut, function(){
+            self.autoUpdateDisabled = !self.autoUpdateDisabled;
+            debug.warn( 'Toggling auto update to ', !self.autoUpdateDisabled );
+            if( self.autoUpdateDisabled ){
                 self._stopUpdate();
-            }else{
-                self._autoUpdate(self.selected);
+            } else {
+                self._autoUpdate( self.selected );
             }
-        });
+        } );
     },
 
-    _autoUpdate : function( model ){
+    _autoUpdate: function( model ){
         if( model && !this.autoUpdateDisabled ){
             this.intervalId = setInterval( function(){
                 model.update();
-            }.bind(this), this.updateInterval );
+            }.bind( this ), this.updateInterval );
         }
     },
 
-    _stopUpdate : function(){
+    _stopUpdate: function(){
         if( this.intervalId ){
             clearInterval( this.intervalId );
         }
     },
 
-    start : function( attrs ){
+    start: function( attrs ){
         debug.debug( 'start', attrs );
         var model = this.add( attrs );
         this.select( model );
@@ -59,24 +61,24 @@ module.exports = Backbone.Collection.extend( {
         return model;
     },
 
-    stop : function(){
+    stop: function(){
         debug.debug( 'stop' );
         var model = this.selected;
-        if(model){
+        if( model ){
             this._stopUpdate();
             this.deselect();
             model
                 .update();
-            this.remove(model);
+            this.remove( model );
         }
         return model;
     },
 
-    onTeardown : function(){
-        debug("#teardown");
+    onTeardown: function(){
+        debug( "#teardown" );
         this.stop();
 
-        Mousetrap.unbind('ctrl+alt+s');
+        Mousetrap.unbind( 'ctrl+alt+s' );
     }
 
 } );
