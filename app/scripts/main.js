6a923cf3d06de29c35435d761146057009178402
Camille Reynders <camille.reynders@gmail.com>
2015-05-18 11:28:44 +0200

    Style changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,13 @@
 require('modernizr');
 require('bootstrap');
 require('bootstrap-material-design/ripples');
-require('imports?jQuery=jquery!exports?jQuery.material!bootstrap-material-design/material' ).init();
+var material = require('imports?jQuery=jquery!exports?jQuery.material!bootstrap-material-design/material' );
 require('bootstrap-validator');
 /* --- */
 
 require('./core/controllers/SetupDebugging')();
 
+material.init();
 
 var debug = require('debug')( 'dpac:app' );
 debug( '*** starting up ***' );
