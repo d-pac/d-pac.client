c0a9d622e62b2eed4bb59665719f27ff74dddbf0
Camille Reynders <camille.reynders@gmail.com>
2015-05-18 11:37:36 +0200

    Drop ripple effect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,11 @@
 /* stand-alones */
 require('modernizr');
 require('bootstrap');
-require('bootstrap-material-design/ripples');
-var material = require('imports?jQuery=jquery!exports?jQuery.material!bootstrap-material-design/material' );
+require('imports?jQuery=jquery!exports?jQuery.material!bootstrap-material-design/material' ).init();
 require('bootstrap-validator');
 /* --- */
 
 require('./core/controllers/SetupDebugging')();
-
-material.init();
 
 var debug = require('debug')( 'dpac:app' );
 debug( '*** starting up ***' );
