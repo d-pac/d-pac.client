e38f00776fcf10204d00f90cb0637932722f2bbe
Camille Reynders <camille.reynders@gmail.com>
2015-06-02 09:01:10 +0200

    Fix #84; do not re-render entire view when selecting a representation

--- app/scripts/assess/views/RepresentationsView.js
+++ app/scripts/assess/views/RepresentationsView.js
@@ -1,42 +1,54 @@
 'use strict';
-var Marionette = require('backbone.marionette');
+var Marionette = require( 'backbone.marionette' );
 var debug = require( 'debug' )( 'dpac:assess.views', '[RepresentationsView]' );
 var tpl = require( './templates/RepresentationsView.hbs' );
-var DetailView = require('./RepresentationDetailView');
+var DetailView = require( './RepresentationDetailView' );
 
-module.exports = Marionette.LayoutView.extend({
-    template : tpl,
+module.exports = Marionette.LayoutView.extend( {
+    template: tpl,
 
-    regions : {
-        representationA : "#representation-A",
-        representationB : "#representation-B"
+    regions: {
+        representationA: "#representation-A",
+        representationB: "#representation-B"
+    },
+
+    ui: {
+        selectedA: '#selected-A',
+        selectedB: '#selected-B'
     },
 
     modelEvents: {
-        'change:selectedRepresentation': 'render'
+        'change:selectedRepresentation': 'showSelected'
     },
 
-    initialize : function(){
-        debug("#initialize");
+    initialize: function(){
+        debug( "#initialize" );
     },
 
-    serializeData: function(raw){
-        var data = {};
+    //serializeData: function(raw){
+    //    var data = {};
+    //    var order = this.model.getSelectedRepresentationOrder();
+    //    console.log('selectedRepresentationOrder', order);
+    //    if(order){
+    //        data["selected"+ order.toUpperCase()] = true;
+    //    }
+    //    return data;
+    //},
+
+    onRender: function(){
+        debug( '#onRender', this.model );
+        this.representationA.show( new DetailView( {
+            model: this.model.getRepresentationByOrder( "a" )
+        } ) );
+        this.representationB.show( new DetailView( {
+            model: this.model.getRepresentationByOrder( "b" )
+        } ) );
+    },
+
+    showSelected: function(){
         var order = this.model.getSelectedRepresentationOrder();
-        console.log('selectedRepresentationOrder', order);
-        if(order){
-            data["selected"+ order.toUpperCase()] = true;
+        if( order ){
+            this.$( this.ui[ "selected" + order.toUpperCase() ] ).removeClass( 'hidden' );
         }
-        return data;
-    },
-
-    onRender : function(){
-        debug('#onRender', this.model);
-        this.representationA.show(new DetailView({
-            model: this.model.getRepresentationByOrder("a")
-        }));
-        this.representationB.show(new DetailView({
-            model: this.model.getRepresentationByOrder("b")
-        }));
     }
-});
+} );
