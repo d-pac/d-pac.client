b5d4f17bf669950c241b6a3325dc9f5ad03f83cb
Camille Reynders <camille.reynders@gmail.com>
2015-08-20 12:31:33 +0200

    DPAC-113 - Allow text-only representations

--- app/scripts/assess/views/RepresentationDetailView.js
+++ app/scripts/assess/views/RepresentationDetailView.js
@@ -3,10 +3,13 @@ var Marionette = require( 'backbone.marionette' );
 var debug = require( 'debug' )( 'dpac:assess.views', '[RepresentationDetailView]' );
 var imageTpl = require( './templates/media/image.hbs' );
 var viewerjsTpl = require( './templates/media/viewerjs.hbs' );
+var htmlTpl = require('./templates/media/html.hbs');
+
 var templates = {
     "image/png": imageTpl,
     "image/jpeg": imageTpl,
-    "application/pdf": viewerjsTpl
+    "application/pdf": viewerjsTpl,
+    "text/html": htmlTpl
 };
 
 module.exports = Marionette.ItemView.extend( {
