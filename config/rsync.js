21b94fadeb16022289bc65cde211cdfae0db6bf9
Camille Reynders <camille.reynders@gmail.com>
2015-09-21 11:17:52 +0200

    Ignore logs in deployment

--- config/rsync.js
+++ config/rsync.js
@@ -9,7 +9,7 @@ module.exports = {
             src                : './dist/', //trailing slash REQUIRED [!]
             dest               : '<%= env.REMOTE_DEST %>',
             host               : ['<%= env.REMOTE_USERNAME %>', '@', '<%= env.REMOTE_HOST %>'].join( '' ),
-            exclude            : ['*-mocks.js', '.git*', '.DS_Store'],
+            exclude            : ['*-mocks.js', '.git*', '.DS_Store', 'logs'],
             dryRun             : false,
             args               : ["--verbose"],
             syncDestIgnoreExcl : true
