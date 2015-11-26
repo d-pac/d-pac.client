280fb32e8431e0b01be5c37379a4e8d960a3b2b0
Camille Reynders <camille.reynders@gmail.com>
2015-11-26 15:19:00 +0100

    Trying to solve very mysterious config loader problem :ghost:

--- app/scripts/core/controllers/LoadConfiguration.js
+++ app/scripts/core/controllers/LoadConfiguration.js
@@ -10,24 +10,45 @@ module.exports = function LoadConfiguration(){
 _.extend( module.exports.prototype, {
     execute: function(){
         debug( '#execute' );
-        debug( 'requesting config with data:', {
-            API_HOST: process.env.API_HOST,
-            APP_VERSION: process.env.APP_VERSION,
-            APP_LANG: process.env.APP_LANG || 'nl'
-        } );
-        konfy.load( {
-            configFile: "scripts/core/config.json",
-            config: {
-                API_HOST: process.env.API_HOST,
-                APP_VERSION: process.env.APP_VERSION,
-                APP_LANG: process.env.APP_LANG || 'nl'
+        this.context.wireValue( 'config', {
+            "api": {
+                "host": process.env.API_HOST,
+                "root": process.env.API_HOST + "api"
+            },
+            "timelogs": {
+                "interval": 5000,
+                "shortcut": "ctrl+alt+s"
+            },
+            "errorlogs": {
+                "shortcut": "ctrl+alt+d"
+            },
+            "app": {
+                "version": process.env.APP_VERSION,
+                "lang": process.env.APP_LANG || 'nl'
+            },
+            "slugs": {
+                "welcome": "tool-welcome"
             }
-        }, function( err,
-                     config ){
-            debug( 'received config:', config );
-            this.context.wireValue( 'config', config );
-            this.context.vent.trigger( 'config:load:completed' );
-        }.bind( this ) );
+        } );
+        this.context.vent.trigger( 'config:load:completed' );
+        //debug( 'requesting config with data:', {
+        //    API_HOST: process.env.API_HOST,
+        //    APP_VERSION: process.env.APP_VERSION,
+        //    APP_LANG: process.env.APP_LANG || 'nl'
+        //} );
+        //konfy.load( {
+        //    configFile: "scripts/core/config.json",
+        //    config: {
+        //        API_HOST: process.env.API_HOST,
+        //        APP_VERSION: process.env.APP_VERSION,
+        //        APP_LANG: process.env.APP_LANG || 'nl'
+        //    }
+        //}, function( err,
+        //             config ){
+        //    debug( 'received config:', config );
+        //    this.context.wireValue( 'config', config );
+        //    this.context.vent.trigger( 'config:load:completed' );
+        //}.bind( this ) );
     }
 } );
 
