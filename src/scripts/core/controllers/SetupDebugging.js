5f3765c2d77d73d01c1867e5e02f7945bc8fdd7d
Camille Reynders <camille.reynders@gmail.com>
2016-04-06 12:19:38 +0200

    Enable backbone inspector debugging

--- src/scripts/core/controllers/SetupDebugging.js
+++ src/scripts/core/controllers/SetupDebugging.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const Backbone = require('backbone');
+const Marionette = require('backbone.marionette');
+
 var debug = require( 'debug' );
 
 module.exports = function SetupDebugging(){
+    if (window.__backboneAgent) { // backbone inspector
+      window.__backboneAgent.handleBackbone(Backbone);
+    }
     debug.config( {
         padLength : 20
     } );
