eca06c87675c0b644a350a175265cf26c31c42f5
Camille Reynders <camille.reynders@gmail.com>
2016-04-13 16:21:57 +0200

    Add password resets

--- src/scripts/core/controllers/SetupHBSHelpers.js
+++ src/scripts/core/controllers/SetupHBSHelpers.js
@@ -7,39 +7,49 @@ var i18n = require( 'i18next' );
 var moment = require( 'moment' );
 
 module.exports = function SetupHBSHelpers(){
-    debug( '#execute' );
+};
+_.extend( module.exports.prototype, {
+    wiring: ['config'],
+    execute: function(){
+        debug( '#execute' );
 
-    function translate( i18n_key,
-                        options ){
-        var context = options.data.root;
-        var opts = (context)
-            ? i18n.functions.extend( options.hash, context )
-            : options.hash;
-        if( options.fn ){
-            opts.defaultValue = options.fn( context );
+        function translate( i18n_key,
+                            options ){
+            var context = options.data.root;
+            var opts = (context)
+                ? i18n.functions.extend( options.hash, context )
+                : options.hash;
+            if( options.fn ){
+                opts.defaultValue = options.fn( context );
+            }
+
+            var result = i18n.t( i18n_key, opts );
+            return new Handlebars.SafeString( result );
         }
 
-        var result = i18n.t( i18n_key, opts );
-        return new Handlebars.SafeString( result );
+        Handlebars.registerHelper( 't', translate );
+        Handlebars.registerHelper( 'toFixed', function( value ){
+            var precision = (arguments.length === 3)
+                ? arguments[ 1 ]
+                : 2;
+            return Number( value ).toFixed( precision );
+        } );
+
+        Handlebars.registerHelper( 'duration', function( seconds ){
+            var d = moment.duration( Number( seconds ), 'seconds' );
+            return _.padLeft( Math.floor( d.asHours() ), 2, "0" ) + ":"
+                + _.padLeft( d.minutes(), 2, "0" ) + ":"
+                + _.padLeft( d.seconds(), 2, "0" );
+        } );
+
+        Handlebars.registerHelper( 'absurl', ( url )=>{
+            const compiled = _.template(i18n.t( url ));
+            return compiled(this.config);
+        } );
+
+        Handlebars.registerHelper( 'encodeURI', function( uri ){
+            return encodeURIComponent( uri );
+        } );
+
     }
-
-    Handlebars.registerHelper( 't', translate );
-    Handlebars.registerHelper( 'toFixed', function( value ){
-        var precision = (arguments.length === 3)
-            ? arguments[ 1 ]
-            : 2;
-        return Number( value ).toFixed( precision );
-    } );
-
-    Handlebars.registerHelper( 'duration', function( seconds ){
-        var d = moment.duration( Number( seconds ), 'seconds' );
-        return _.padLeft( Math.floor( d.asHours() ), 2, "0" ) + ":"
-            + _.padLeft( d.minutes(), 2, "0" ) + ":"
-            + _.padLeft( d.seconds(), 2, "0" );
-    } );
-
-    Handlebars.registerHelper( 'encodeURI', function( uri ){
-        return encodeURIComponent( uri );
-    } );
-
-};
+});
